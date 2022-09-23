68c0da77469420600ac1384a428f1e197095b549
Joris <jorisdouven@hotmail.com>
2022-09-23 16:53:08 +0200

    add tailwind

--- gradport/gulpfile.js
+++ gradport/gulpfile.js
@@ -1,4 +1,4 @@
-const {series, parallel, watch, src, dest} = require('gulp');
+const { series, parallel, watch, src, dest } = require('gulp');
 const pump = require('pump');
 
 // gulp plugins and utils
@@ -10,87 +10,102 @@ const uglify = require('gulp-uglify');
 const beeper = require('beeper');
 const zip = require('gulp-zip');
 
+// tailwind
+const tailwindcss = require('tailwindcss');
+const tailwindConfig = require('./tailwind.config');
+
 // postcss plugins
 const easyimport = require('postcss-easy-import');
 const autoprefixer = require('autoprefixer');
 const cssnano = require('cssnano');
 
 function serve(done) {
-    livereload.listen();
-    done();
+  livereload.listen();
+  done();
 }
 
 function handleError(done) {
-    return function (err) {
-        if (err) {
-            beeper();
-        }
-        return done(err);
-    };
-};
+  return function (err) {
+    if (err) {
+      beeper();
+    }
+    return done(err);
+  };
+}
 
 function hbs(done) {
-    pump([
-        src(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs']),
-        livereload()
-    ], handleError(done));
+  pump(
+    [src(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs']), livereload()],
+    handleError(done)
+  );
 }
 
 function css(done) {
-    pump([
-        src('assets/css/screen.css', {sourcemaps: true}),
-        postcss([
-            easyimport,
-            autoprefixer(),
-            cssnano()
-        ]),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+  pump(
+    [
+      src('assets/css/*.css', { sourcemaps: true }),
+      postcss([
+        easyimport,
+        tailwindcss(tailwindConfig),
+        autoprefixer(),
+        cssnano(),
+      ]),
+      dest('assets/built/', { sourcemaps: '.' }),
+      livereload(),
+    ],
+    handleError(done)
+  );
 }
 
 function js(done) {
-    pump([
-        src([
-            'assets/js/lib/*.js',
-            'assets/js/main.js'
-        ], {sourcemaps: true}),
-        concat('main.min.js'),
-        uglify(),
-        dest('assets/built/', {sourcemaps: '.'}),
-        livereload()
-    ], handleError(done));
+  pump(
+    [
+      src(['assets/js/lib/*.js', 'assets/js/main.js'], { sourcemaps: true }),
+      concat('main.min.js'),
+      uglify(),
+      dest('assets/built/', { sourcemaps: '.' }),
+      livereload(),
+    ],
+    handleError(done)
+  );
 }
 
 function lint(done) {
-    pump([
-        src(['assets/css/**/*.css', '!assets/css/vendor/*']),
-        gulpStylelint({
-            fix: true,
-            reporters: [
-                {formatter: 'string', console: true}
-            ]
-        }),
-        dest('assets/css/')
-    ], handleError(done));
+  pump(
+    [
+      src(['assets/css/**/*.css', '!assets/css/vendor/*']),
+      gulpStylelint({
+        fix: true,
+        reporters: [{ formatter: 'string', console: true }],
+      }),
+      dest('assets/css/'),
+    ],
+    handleError(done)
+  );
 }
 
 function zipper(done) {
-    const filename = require('./package.json').name + '.zip';
+  const filename = require('./package.json').name + '.zip';
 
-    pump([
-        src([
-            '**',
-            '!node_modules', '!node_modules/**',
-            '!dist', '!dist/**',
-            '!yarn-error.log'
-        ]),
-        zip(filename),
-        dest('dist/')
-    ], handleError(done));
+  pump(
+    [
+      src([
+        '**',
+        '!node_modules',
+        '!node_modules/**',
+        '!dist',
+        '!dist/**',
+        '!yarn-error.log',
+      ]),
+      zip(filename),
+      dest('dist/'),
+    ],
+    handleError(done)
+  );
 }
 
-const hbsWatcher = () => watch(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs'], hbs);
+const hbsWatcher = () =>
+  watch(['*.hbs', 'partials/**/*.hbs', 'members/**/*.hbs'], hbs);
 const cssWatcher = () => watch('assets/css/**/*.css', css);
 const jsWatcher = () => watch('assets/js/**/*.js', js);
 const watcher = parallel(hbsWatcher, cssWatcher, jsWatcher);
---
+++ gradport/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['*.hbs', './partials/*.hbs'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
